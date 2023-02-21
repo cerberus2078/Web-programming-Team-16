4d601f97cdfbc33fabff01e7aa9f2cc54322b7fd
Sara-Sofia Paananen <111874242+cerberus2078@users.noreply.github.com>
2023-02-21 14:23:22 +0200

    newuser php stuff

--- db1.php
+++ db1.php
@@ -1,14 +1,11 @@
 <?php
 // Sara's db table
 $servername = "db";
-$username = "";
-$password = "";
+$username = "festevent";
+$password = "team16";
 $dbname = "festevent";
 
-//creating connection
 $conn =  new mysqli($servername,$username,$password,$dbname);
-
-//check the connection
 
 if ($conn->connect_error){
     die("connection failed:".$conn->connect_error);
