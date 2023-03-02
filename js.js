b2ffdc93fcdd994350c3bf41c748f7354d423dc9
Sara-Sofia Paananen <111874242+cerberus2078@users.noreply.github.com>
2023-03-02 18:52:23 +0200

    java stuff

--- js.js
+++ js.js
@@ -1,15 +1,15 @@
 function crud(){
     let fname=(document.form1.fname.value).trim();
-    if(fname.length<5){
-        alert("First Name must have at least 5 characters");
+    if(fname.length<3){
+        alert("First Name must have at least 3 characters");
         return false;
     }
 }
 
 function fname(){
     let fname=(document.form1.fname.value).trim();
-    if(fname.length<5){
-        alert("First Name must have at least 5 characters");
+    if(fname.length<3){
+        alert("First Name must have at least 3 characters");
         return false;
     }
 }
